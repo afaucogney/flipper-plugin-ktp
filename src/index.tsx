61264c1ddd512a295f524b20a1eb0cec45b3b821
Anthony Faucogney <anthony.faucogney@niji.fr>
2020-12-05 15:07:12 +0100

    wip selection

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {PluginClient, usePlugin, createState, useValue, Layout} from 'flipper-plugin';
-import { Tree, Input } from 'antd';
+import { Tree, Typography } from 'antd';
 import {
   HomeOutlined,
   AndroidOutlined,
@@ -8,6 +8,7 @@ import {
   PartitionOutlined,
   MehOutlined,
 } from '@ant-design/icons';
+import {ManagedDataInspector, DetailSidebar} from 'flipper';
 
 type Data = {
   key: string;
@@ -24,6 +25,7 @@ type Events = {
 // API: https://fbflipper.com/docs/extending/flipper-plugin#pluginclient
 export function plugin(client: PluginClient<Events, {}>) {
     const data = createState<Record<string, Data>>({}, {persist: 'data'});
+    const selectedID = createState<string | null>(null, {persist: 'selection'});
 
     client.onMessage('newData', (newData) => {
         data.update((draft) => {
@@ -37,27 +39,18 @@ export function plugin(client: PluginClient<Events, {}>) {
             data.set({});
         },
     });
-    console.log(data);
 
-    const dataList = [];
-    const generateList = data => {
-        for (let i = 0; i < data.length; i++) {
-            const node = data[i];
-            const { key } = node;
-            dataList.push({ key, title: key });
-            if (node.children) {
-                generateList(node.children);
-            }
-        }
-    };
-    if (Object.keys(data).length < 1) {
-        generateList(data[0]);
-    }
-    console.log(dataList);
+      function setSelection(id: number) {
+        selectedID.set('' + id);
+        console.log("selected", id)
+      }
 
+    console.log("selected", selectedID)
+    console.log("data", data)
     return {
         data,
-        dataList,
+        selectedID,
+        setSelection,
     };
 }
 
@@ -66,10 +59,13 @@ export function plugin(client: PluginClient<Events, {}>) {
 export function Component() {
     const instance = usePlugin(plugin);
     const data = useValue(instance.data);
-    const dataList = useValue(instance.dataList);
-    const { Search } = Input;
+    const selectedID = useValue(instance.selectedID);
 
-    console.log(data[0])
+    const onSelect = (selectedKeys, info) => {
+        console.log('selected', selectedKeys);//, info);
+        instance.setSelection(selectedKeys[0])
+    };
+
     if (Object.keys(data).length < 1) {
         return (
             <Layout.ScrollContainer>
@@ -77,15 +73,48 @@ export function Component() {
         );
     } else {
          return (
+         <>
             <Layout.ScrollContainer>
-                <Search style={{ marginBottom: 8 }} placeholder="Search" onChange={this.onChange} />
                 <Tree
-                    showIcon
+                    // showIcon
                     defaultExpandAll
                     autoExpandParent
+                    onSelect={onSelect}
                     treeData={[data[0]]}
                 />
             </Layout.ScrollContainer>
+              <DetailSidebar>
+                {selectedID && renderSidebar(data[0],selectedID)}
+              </DetailSidebar>
+              </>
          );
+    }
+
+    function renderSidebar(row: Row, selectedId :number) {
+        console.log("row: ", row)
+        console.log("id: ", selectedId)
+        console.log("row: ", filter(row,selectedId))
+      return (
+        <Layout.Container gap pad>
+          <Typography.Title level={4}>Extras</Typography.Title>
+          <ManagedDataInspector data={row} expandRoot={true} />
+        </Layout.Container>
+      );
+      }
+
+    function filter(array, text) {
+        const getNodes = (result, object) => {
+            if (object.key === text) {
+                result.push(object);
+                return result;
+            }
+            if (Array.isArray(object.children)) {
+                const nodes = object.children.reduce(getNodes, []);
+                if (nodes.length) result.push({ ...object, nodes });
+            }
+            return result;
+        };
+
+        return array.children.reduce(getNodes, []);
     }
 }
